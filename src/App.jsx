1cdc1d6bcf5d984459674a4ce100fc5804537245
Leonardo Favale <ldfavale@gmail.com>
2023-10-20 16:02:25 -0300

    Slider REsponsive

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <>
       <div className='  h-44 '></div>
       <HeroSlider
-      height={"calc(100vh - 210px)"}
+
       style={{minHeight: "650px"}}
       autoplay
       accessibility={{
@@ -40,29 +40,34 @@ function App() {
       }}
     >
       <Overlay>
+        {/* Overlay Container */}
         <div className='flex  flex-col items-center justify-center h-[100%]'>
-          <div className='h-10'></div>
-          <h1 className=' font-aphrodite text-[5rem] slider-title mb-4 text-white [text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.6);] '> Dom & Danai</h1>
+          {/* <div className='h-10 '></div> */}
 
-          <span className='font-now  text-white text-xl slider-subtitle mb-12'>17 FEB 2024 </span>
+          {/* Main Title */}
+          <h1 className=' font-aphrodite text-[2.5rem] mb-8 md:text-6xl lg:text-[5rem] slider-title md:mb-8 text-white [text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.6);] text-center '> Dom & Danai</h1>
 
-          <div className=' max-w-2xl bg-[rgba(178,201,211,0.8)] '>
+          {/* Wedding Date */}
+          <span className='font-now  text-white  text-[0.625rem] md:text-xs  lg:text-[1.125rem] slider-subtitle mb-12 tracking-widest'>17 FEB 2024 </span>
+
+          {/* Countdown Counter */}
+          <div className=' max-w-2xl bg-[rgba(178,201,211,0.8)]  '>
             <div className=' flex flex-row'>
-              <div className="p-8 py-5 w-44 flex flex-col  justify-center items-center">
-                <h2 className='font-aphrodite text-white text-7xl mb-7'>177</h2>
+              <div className="p-8 py-7 lg:py-5 w-16 sm:w-20 md:w-44 flex flex-col  justify-center items-center">
+                <h2 className='font-aphrodite text-white text-3xl lg:text-5xl xl:text-7xl mb-2 lg:mb-4 xl:mb-6'>177</h2>
                 <span className='font-now text-white text-sm'>DIAS</span>
               </div>
-              <div className="p-8 py-5 w-44 flex flex-col  justify-center items-center">
-                <h2 className='font-aphrodite text-white text-7xl mb-7'>17</h2>
+              <div className="p-8 py-5  w-16 sm:w-20 md:w-44 flex flex-col  justify-center items-center">
+                <h2 className='font-aphrodite text-white text-3xl lg:text-5xl xl:text-7xl mb-2 lg:mb-4 xl:mb-6'>17</h2>
                 <span className='font-now text-white text-sm'>HORAS</span>
               </div>
-              <div className="p-8 py-5 w-44 flex flex-col  justify-center items-center">
-                <h2 className='font-aphrodite text-white text-7xl mb-7'>54</h2>
-                <span className='font-now text-white text-sm'>MINUTOS</span>
+              <div className="p-8 py-5  w-16 sm:w-20 md:w-44 flex flex-col  justify-center items-center">
+                <h2 className='font-aphrodite text-white text-3xl lg:text-5xl xl:text-7xl mb-2 lg:mb-4 xl:mb-6'>54</h2>
+                <span className='font-now text-white text-sm'>MINS</span>
               </div>
-              <div className="p-8 py-5 w-44 flex flex-col  justify-center items-center">
-                <h2 className='font-aphrodite text-white text-7xl mb-7'>29</h2>
-                <span className='font-now text-white text-sm'>SEGUNDOS</span>
+              <div className="p-8 py-5 w-16  sm:w-20 md:w-44 flex flex-col  justify-center items-center">
+                <h2 className='font-aphrodite text-white text-3xl lg:text-5xl xl:text-7xl mb-2 lg:mb-4 xl:mb-6'>29</h2>
+                <span className='font-now text-white text-sm'>SEGS</span>
               </div>
             </div>
           </div>
