c0416aa7f8e0ad83364a91e833f1655a6984ddb0
Leonardo Favale <ldfavale@gmail.com>
2023-10-27 18:24:55 -0300

    RSVP Form (Not functional)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Slider from './components/Slider'
 import Header from './components/Header'
 import OurStorySection from './components/OurStorySection'
 import ThePlaceSection from './components/ThePlaceSection'
+import ReservationSection from './components/ReservationSection'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,9 +14,13 @@ function App() {
       <Slider />
       <OurStorySection />
       <ThePlaceSection/>
+      <ReservationSection/>
+
       {/* <a href="https://www.flaticon.es/iconos-gratis/paloma" title="paloma iconos">Paloma iconos creados por shmai - Flaticon</a> */}
       {/* /* background: -webkit-gradient(linear, left top, left bottom, color-stop(30%, #b2c9d3), to(transparent)); */}
-    </>
+      {/* <a href="https://www.freepik.com/icons/wedding">Icon by Freepik</a> */}
+      {/* <a href="https://www.freepik.com/icons/wedding">Icon by Freepik</a> */}
+       </>
   )
 }
 
--- src/components/ThePlaceSection.jsx
+++ src/components/ThePlaceSection.jsx
@@ -4,18 +4,21 @@ import map from '../assets/images/maps.png'
 
 function ThePlaceSection() {
   return (
-    <div className='flex flex-col  h-[728px] sm:h-[728px]  '>
-      <div className="h-[70%] bg-[#DAEEEC]">
-        <div className="h-full bg-[url('./assets/images/smiling.png')] bg-no-repeat  bg-[1216px_810px_]  bg-[center_top_-80px] sm:bg-[center_top_-80px] celebrate ">
+    <div className='flex flex-col pb-20'>
+      <div className=" bg-[#DAEEEC]">
+        <div className="h-[510px] bg-[url('./assets/images/smiling.png')] bg-no-repeat  bg-[1216px_810px_]  bg-[center_top_-80px] sm:bg-[center_top_-80px] celebrate ">
              {/* Main Title bg-[912px_607px_] bg-[608px_405px_] */}
-
         </div>
       </div>
-          <h1 className=' font-aphrodite text-4xl sm:text-5xl md:text-6xl     text-white text-center   -mt-[23rem] z-10'> Celebrate Our Love</h1>
-           {/* Wedding Date */}
-           <h3 className=' font-now  text-white text-center  text-lg md:text-xl  lg:text-2xl [letter-spacing:5px] mt-4 z-10'>17 FEB 2024 </h3>
-           {/* https://www.google.com/maps/place/Estancia+Aguaverde/@-34.843679,-54.87884,17z/data=!4m9!3m8!1s0x95751be35b57acad:0xbbf7e6f2a152c88e!5m2!4m1!1i2!8m2!3d-34.8427182!4d-54.8790803!16s%2Fg%2F11clgd_8_g?hl=es&entry=ttu */}
-    <div className='h-96 mt-28 flex flex-col sm:flex-row  sm:flex-wrap p-4 items-center sm:justify-center'>
+
+      <h1 className=' font-aphrodite text-4xl sm:text-5xl md:text-6xl     text-white text-center   -mt-[23rem] z-10'> Celebrate Our Love</h1>
+
+      {/* Wedding Date */}
+      <h3 className=' font-now  text-white text-center  text-lg md:text-xl  lg:text-2xl [letter-spacing:5px] mt-4 z-10'>17 FEB 2024 </h3>
+
+      {/* https://www.google.com/maps/place/Estancia+Aguaverde/@-34.843679,-54.87884,17z/data=!4m9!3m8!1s0x95751be35b57acad:0xbbf7e6f2a152c88e!5m2!4m1!1i2!8m2!3d-34.8427182!4d-54.8790803!16s%2Fg%2F11clgd_8_g?hl=es&entry=ttu */}
+
+      <div className=' mt-28 flex flex-col sm:flex-row  sm:flex-wrap p-4 items-center sm:justify-center'>
 
         <div className='bg-white p-3  order-3 lg:order-1 flex flex-col max-w-[428px] sm:max-w-xs  mx-2 md:mx-4 items-center mb-10 sombrita'>
           {/* <h3 className='font-aphrodite text-lg sm:text-xl text-center mb-10 before:relative before:top-9 before:left-[50%]   before:-translate-x-1/2 before:w-32 before:h-[2px] before:flex before:bg-gray-300 '> The Place</h3> */}
